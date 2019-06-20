2b6563ac5f97086a5f5783c418ffeb0be493d23f
Constance Chen <constance.chen.3@gmail.com>
2019-06-20 09:11:42 -0700

    Update Prettier settings because I'm an opinionated jerk

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,21 @@
-import React from "react";
-import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
-import { SearchProvider, Results, SearchBox } from "@elastic/react-search-ui";
-import { Layout } from "@elastic/react-search-ui-views";
+import React from 'react';
+import AppSearchAPIConnector from '@elastic/search-ui-app-search-connector';
+import { SearchProvider, Results, SearchBox } from '@elastic/react-search-ui';
+import { Layout } from '@elastic/react-search-ui-views';
 
-import "@elastic/react-search-ui-views/lib/styles/styles.css";
+import '@elastic/react-search-ui-views/lib/styles/styles.css';
 
 const connector = new AppSearchAPIConnector({
-  searchKey: "search-371auk61r2bwqtdzocdgutmg",
-  engineName: "search-ui-examples",
-  hostIdentifier: "host-2376rb"
+  searchKey: 'search-371auk61r2bwqtdzocdgutmg',
+  engineName: 'search-ui-examples',
+  hostIdentifier: 'host-2376rb',
 });
 
 export default function App() {
   return (
     <SearchProvider
       config={{
-        apiConnector: connector
+        apiConnector: connector,
       }}
     >
       <div className="App">
