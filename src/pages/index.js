62179a4fbd5fd02ad58a32b7ec358dadf28164ec
Constance Chen <constance.chen.3@gmail.com>
2019-06-20 09:09:13 -0700

    Add basic Search UI setup/example

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,29 @@
-import React from "react"
+import React from "react";
+import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
+import { SearchProvider, Results, SearchBox } from "@elastic/react-search-ui";
+import { Layout } from "@elastic/react-search-ui-views";
 
-export default () => <div>Hello world!</div>
+import "@elastic/react-search-ui-views/lib/styles/styles.css";
+
+const connector = new AppSearchAPIConnector({
+  searchKey: "search-371auk61r2bwqtdzocdgutmg",
+  engineName: "search-ui-examples",
+  hostIdentifier: "host-2376rb"
+});
+
+export default function App() {
+  return (
+    <SearchProvider
+      config={{
+        apiConnector: connector
+      }}
+    >
+      <div className="App">
+        <Layout
+          header={<SearchBox />}
+          bodyContent={<Results titleField="title" urlField="nps_link" />}
+        />
+      </div>
+    </SearchProvider>
+  );
+}
